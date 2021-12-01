04d2562fea97b6009fa9ae74bd5669a5aca582dc
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-12-02 08:27:05 +0900

    migration complete

---
+++ pybo/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from pybo.models import Question, Answer, Comment
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question  # 사용할 모델
+        fields = ['subject', 'content']  # QuestionForm에서 사용할 Question 모델의 속성
+        labels = {
+            'subject': '제목',
+            'content': '내용',
+        }
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        labels = {
+            'content': '댓글내용',
+        }
